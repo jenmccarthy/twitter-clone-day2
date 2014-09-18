f9546974f8bf5b44422ec065e3e629a51670c6ec
jenmccarthy <jennifernward@gmail.com>
2014-09-18 11:10:08 -0700

    get devise working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
- root to: 'users#index'
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+ root to: 'tweets#index'
 end

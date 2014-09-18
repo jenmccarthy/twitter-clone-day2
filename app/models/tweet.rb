084fde4aa9244da0477f197ffc15379e9b4923ec
jenmccarthy <jennifernward@gmail.com>
2014-09-18 10:54:40 -0700

    a tweet belongs to a user

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ActiveRecord::Base
   validates :status, presence: :true
   validates :user_id, presence: :true
 
+  belongs_to :user
+
 end
--- spec/tweet_spec.rb
+++ spec/tweet_spec.rb
@@ -4,4 +4,6 @@ describe Tweet do
   it { should validate_presence_of :status }
   it { should validate_presence_of :user_id }
 
+  it { should belong_to :user }
+
 end

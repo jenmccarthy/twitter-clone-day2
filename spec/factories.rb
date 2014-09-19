3f7b3cb7ceb7f0db2c2c29cd88cd51b6677dbb6d
jenmccarthy <jennifernward@gmail.com>
2014-09-19 09:45:50 -0700

    add migration for relationships indexes

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,8 @@
 FactoryGirl.define do
 
-  factory(:user) do
-    email('name@email.com')
-    password('12345678')
-    password_confirmation('12345678')
+  factory :user do |user|
+    sequence(:email) { |n| "user#{n}@factory.com" }
+    user.password{ "secret12" }
   end
 
-  # factory(:user2) do
-  #   email('nametoo@email.com')
-  #   password('asdfghjk')
-  #   password_confirmation('asdfghjk')
-  # end
 end

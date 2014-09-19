340a649927f0788b60dbc565f773bfd89eb852fe
jenmccarthy <jennifernward@gmail.com>
2014-09-19 11:56:01 -0700

    make fixed top and bottom navbars

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,7 +2,7 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
-  scope :show_followed_tweets, -> (current_user) { where(followed_id: current_user.id) }
+  # scope :show_followed_tweets, -> (current_user) { where(followed_id: current_user.id) }
 
   # def show_followed_tweets
   #   if current_user.id == follower_id

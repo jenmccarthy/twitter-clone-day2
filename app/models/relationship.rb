870ea6c9546aa397946ea019164dbfe423b15d92
jenmccarthy <jennifernward@gmail.com>
2014-09-19 11:07:09 -0700

    allow followed tweets to print on users show page when logged in

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,14 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  scope :show_followed_tweets, -> (current_user) { where(followed_id: current_user.id) }
+
+  # def show_followed_tweets
+  #   if current_user.id == follower_id
+  #     current_user.followed.each do |followed|
+  #       followed.status
+  #     end
+  #   end
+  # end
 end
